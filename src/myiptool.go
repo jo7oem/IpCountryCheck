153d3ead4ed5a633bd6e64ef94edee01fa169bb9
su-2 <su-2@ume-ch.net>
2016-12-13 15:44:07 +0900

    [clean] コードの整理

--- src/myiptool.go
+++ src/myiptool.go
@@ -9,15 +9,16 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"golang.org/x/net/webdav"
 )
 
-type ipdate struct {
-	IPaddr string
+type IpData struct {
+	IpAddr string
 	Mask   int
 }
-type ipDataCountry struct {
+type IpDataCountry struct {
 	Country string
-	ipdate
+	IpData
 }
 
 func GetHttp(url string) (html string, err error) {
@@ -37,21 +38,21 @@ func Atoi(s string) int {
 	i, _ := strconv.Atoi(s)
 	return i
 }
-func SplitIpData(instrings string) (IpDataList ipDataCountry, e error) {
-	buf := strings.Split(instrings, "|")
+func SplitIpData(strList string) (IpDataList IpDataCountry, e error) {
+	buf := strings.Split(strList, "|")
 	//buf="arin|US|ipv4|64.187.240.0|4096|20121210|assigned|45"
 	//buf="arin|US|ipv6|2607:c880::|32|20161110|allocated|5d1f"
 	if buf[1] == "" {
 		e := errors.New("NoCountry")
 		return IpDataList, e
 	}
-	IpDataList.Country, IpDataList.IPaddr, IpDataList.Mask = buf[1], buf[3], Atoi(buf[4])
+	IpDataList.Country, IpDataList.IpAddr, IpDataList.Mask = buf[1], buf[3], Atoi(buf[4])
 	return IpDataList, e
 }
-func datamakeIp(instrings *[]string, kyeword string, ch chan *[]ipDataCountry) {
-	var DataList []ipDataCountry
-	for _, str := range *instrings {
-		if strings.Contains(str, kyeword) {
+func dataMakeIp(strList *[]string, keyStr string, ch chan *[]IpDataCountry) {
+	var DataList []IpDataCountry
+	for _, str := range *strList {
+		if strings.Contains(str, keyStr) {
 			buf, err := SplitIpData(str)
 			if err != nil {
 				continue
@@ -62,41 +63,35 @@ func datamakeIp(instrings *[]string, kyeword string, ch chan *[]ipDataCountry) {
 	DataListP := &DataList
 	ch <- DataListP
 }
-func GetIpCountryList(url string) (iv4list, iv6list *[]ipDataCountry, err error) {
+func GetIpCountryList(url string) (iv4list, iv6list *[]IpDataCountry, err error) {
 	htdata, err := GetHttp(url)
 	if err != nil {
 		return nil, nil, err
 	}
 	gg := strings.Split(htdata, "\n")
 
-	iv4ch := make(chan *[]ipDataCountry)
-	iv6ch := make(chan *[]ipDataCountry)
-	go datamakeIp(&gg, "ipv4", iv4ch)
-	go datamakeIp(&gg, "ipv6", iv6ch)
+	iv4ch := make(chan *[]IpDataCountry)
+	iv6ch := make(chan *[]IpDataCountry)
+	go dataMakeIp(&gg, "ipv4", iv4ch)
+	go dataMakeIp(&gg, "ipv6", iv6ch)
 	iv4list = <-iv4ch
 	iv6list = <-iv6ch
 	return iv4list, iv6list, err
 }
-
-/*func newww()  {
-
-}
-*/
 func init(){
 	cpus := runtime.NumCPU()
 	runtime.GOMAXPROCS(cpus)
 }
 func main() {
 	url := "http://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest"
-
 	iv4l, iv6l, err := GetIpCountryList(url)
 	if err != nil {
 		panic("!!!WARN!!!")
 	}
 	for _, ss := range *iv4l {
-		fmt.Printf("%s %s %d\n", ss.Country, ss.IPaddr, ss.Mask)
+		fmt.Printf("%s %s %d\n", ss.Country, ss.IpAddr, ss.Mask)
 	}
 	for _, ss := range *iv6l {
-		fmt.Printf("%s %s %d\n", ss.Country, ss.IPaddr, ss.Mask)
+		fmt.Printf("%s %s %d\n", ss.Country, ss.IpAddr, ss.Mask)
 	}
 }
