fbc21125a7f43beb3ddd2cfdb75ce9c930a9c4f1
jo7oem <jo7oem@gmail.com>
2017-02-07 03:18:33 +0900

    fix

--- src/clifront.go
+++ src/clifront.go
@@ -1,29 +1,34 @@
 package main
 
 import (
+	"./ipaddrtools"
 	"fmt"
 	"github.com/codegangsta/cli"
-	"os"
-	"./ipaddrtools"
 	"log"
+	"os"
 )
+
 func main() {
-	app:=cli.NewApp()
-	app.Name="IP CLI TOOL"
-	app.Author="jo7oem"
-	app.Usage="IPアドレス関連ツール"
-	app.Version="0.1.1"
-	
-	app.Before=func(c *cli.Context) error{
+	app := cli.NewApp()
+	app.Name = "IP CLI TOOL"
+	app.Author = "jo7oem"
+	app.Usage = "IPアドレス関連ツール"
+	app.Version = "0.1.1"
+
+	app.Before = func(c *cli.Context) error {
 		return nil
 	}
-	app.After=func(c *cli.Context) error{
+	app.After = func(c *cli.Context) error {
 		return nil
 	}
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
 			Name:  "dryrun, d", // 省略指定 => d
 			Usage: "グローバルオプション 未実装",
+		},
+		cli.BoolFlag{
+			Name:  "stdin, s", // 省略指定 => d
+			Usage: "標準入力受付",
 		},
 	}
 	app.Commands = []cli.Command{
@@ -54,57 +59,69 @@ func main() {
 		},
 		{
 			Name:    "convPTS",
-			Aliases: []string{"c"},
+			Aliases: []string{"PTS"},
 			Usage:   "BindのIP逆引き形式に変換する",
 			Action:  ConvPTS,
 		},
 	}
 	app.Run(os.Args)
 }
-func Check_Ipaddr(c *cli.Context){
-	for _,s:=range joinArg(c) {
-		fmt.Printf("%s:v4=%s:v6=%s\n",s,ipaddrtools.Checkip4addr(s),ipaddrtools.Checkip6addr(s))
+func Check_Ipaddr(c *cli.Context) {
+	for _, s := range joinArg(c) {
+		fmt.Printf("%s:v4=%s:v6=%s\n", s, ipaddrtools.Checkip4addr(s), ipaddrtools.Checkip6addr(s))
 	}
 }
-func joinArg(c *cli.Context)[]string{
+func joinArg(c *cli.Context) []string {
+	var sbuf string
 	var str []string
-	str=append(str,c.Args().First())
-	str=append(str,c.Args().Tail()...)
+	sss:=c.GlobalBool("stdin")
+	if sss {
+		for {
+			_, e := fmt.Scanln(&sbuf)
+			if e != nil {
+				break
+			}
+			str = append(str, sbuf)
+		}
+	} else {
+		str = append(str, c.Args().First())
+		str = append(str, c.Args().Tail()...)
+	}
 	return str
 }
-func isIpaddr(c *cli.Context){
-	s:=c.Args().First()
-	if ipaddrtools.Checkip4addr(s)||ipaddrtools.Checkip6addr(s) {
-	}else{
-		cli.NewExitError("no ip addr",1)
+func isIpaddr(c *cli.Context) {
+	s := c.Args().First()
+	if ipaddrtools.Checkip4addr(s) || ipaddrtools.Checkip6addr(s) {
+	} else {
+		cli.NewExitError("no ip addr", 1)
 	}
 }
-func ExtendAddr(c *cli.Context){
-	for _,s:=range joinArg(c){
-		ea,err:=ipaddrtools.Extendip6addr(s,true,false)
-		if err !=nil{
+func ExtendAddr(c *cli.Context) {
+	for _, s := range joinArg(c) {
+		ea, err := ipaddrtools.Extendip6addr(s, true, false)
+		if err != nil {
 			log.Fatalln(err)
-		}else{
+		} else {
 			fmt.Println(ea)
 		}
 	}
 }
-func SimpleV6(c *cli.Context){
-	for _,s:=range joinArg(c){
-		ea,err:=ipaddrtools.SimplifyIp6(s)
-		if err !=nil{
+func SimpleV6(c *cli.Context) {
+	for _, s := range joinArg(c) {
+		ea, err := ipaddrtools.SimplifyIp6(s)
+		if err != nil {
 			log.Fatalln(err)
-		}else{
+		} else {
 			fmt.Println(ea)
 		}
 	}
 }
-func ConvPTS(c *cli.Context){
-	for _,s:=range joinArg(c){
-		ea,err:=ipaddrtools.ModePTS(s)
-		if err !=nil{
+func ConvPTS(c *cli.Context) {
+	for _, s := range joinArg(c) {
+		ea, err := ipaddrtools.ModePTS(s)
+		if err != nil {
 			log.Fatalln(err)
-		}else{
+		} else {
 			fmt.Println(ea)
 		}
 	}
